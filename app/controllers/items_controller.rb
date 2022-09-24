5704c77d49b62c0250d93064d2469af434ef7cfa
allfiction100806210913 <lili10080621@gmail.com>
2022-09-24 18:06:02 +0900

    update: product editing function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :index
+    end
   end
 
 
