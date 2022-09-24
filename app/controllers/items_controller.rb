46d1be5b80b3b1389789287f43c00fb74789f4b3
allfiction100806210913 <lili10080621@gmail.com>
2022-09-24 21:26:38 +0900

    update: edit code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_signed_in, except: [:index, :show]
+  before_action :carent_user, except:[:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -28,12 +29,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
-      render :index
+      render :edit
     end
   end
 
@@ -45,11 +46,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_text, :price, :image, :status_id, :shipping_status_id, :prefecture_id, :scheduled_delivery_id, :category_id).merge(user_id: current_user.id)
   end
 
-
-
   def move_to_signed_in
     unless user_signed_in?
       redirect_to '/users/sign_in'
+    end
+  end
+
+  def carent_user
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
     end
   end
 end
